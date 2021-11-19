71b083965aa59df7f7ecd79082b5b44525a97a69
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2021-11-19 05:47:01 +0100

    Add Solution: CountingValleys

---
+++ Warm-Up-Challenges/CountingValleys.php
@@ -0,0 +1,41 @@
+<?php
+
+define('STDIN', fopen('CountingValleys.in', 'r'));
+define('STDOUT', dirname(__DIR__) . '/Outputs/CountingValleys.out');
+putenv(sprintf('OUTPUT_PATH=%s', STDOUT));
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys($steps, $path) {
+    $level = 0;
+    $count = 0;
+
+    for($i=0; $i<$steps; $i++) {
+        if($path[$i] == 'U') {
+            $level += 1;
+        } else if($path[$i] == 'D') {
+            $level -= 1;
+        }
+
+        if($level == 0 && $path[$i] == 'U') {
+            $count += 1;
+        }
+    }
+
+    return $count;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+$steps = intval(trim(fgets(STDIN)));
+$path = rtrim(fgets(STDIN), "\r\n");
+$result = countingValleys($steps, $path);
+
+fwrite($fptr, $result . "\n");
+fclose($fptr);
